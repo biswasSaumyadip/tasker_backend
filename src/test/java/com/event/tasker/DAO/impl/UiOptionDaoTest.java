455c9ce9abd76fc812383d82614414ee964aa688
saumyadip biswas <saumyadip11@gmail.com>
2025-05-25 22:16:27 +0530

    fix: Check style issue fixes

--- src/test/java/com/event/tasker/DAO/impl/UiOptionDaoTest.java
+++ src/test/java/com/event/tasker/DAO/impl/UiOptionDaoTest.java
@@ -1,9 +1,14 @@
 package com.event.tasker.DAO.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
--- src/test/java/com/event/tasker/model/UiOptionTest.java
+++ src/test/java/com/event/tasker/model/UiOptionTest.java
@@ -1,6 +1,7 @@
 package com.event.tasker.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
