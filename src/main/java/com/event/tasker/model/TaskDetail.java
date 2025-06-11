a8af6be2aa4c983004cc0adca2e36f52f4fda128
saumyadip biswas <saumyadip11@gmail.com>
2025-06-11 21:34:53 +0530

    feat: TaskDetail model added

---
+++ src/main/java/com/event/tasker/model/TaskDetail.java
@@ -0,0 +1,28 @@
+package com.event.tasker.model;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TaskDetail {
+  private String id, title, description, assignedTo, parentId;
+
+  @Builder.Default private boolean completed = false;
+
+  @Builder.Default private Task.Priority priority = Task.Priority.LOW;
+
+  private Instant dueDate;
+  private List<String> tags;
+
+  @Builder.Default private List<Attachment> attachments = new ArrayList<>();
+  private List<String> teamMembers;
+}
